389b5275e790b533f1a1b61d0651ec3d780e1649
Hajin74 <yoonmij9974@naver.com>
2023-10-08 22:44:36 +0900

    Controller 리팩토링

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.SocketOption;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -22,15 +24,18 @@ public class ArticleController {
 
     @PostMapping("/articles/create") // URL 요청 접수
     public String createArticle(ArticleForm form) { // 폼 데이터를 dto로 받기
-        System.out.println(form.toString());
+//        System.out.println(form.toString());
+        log.info(form.toString());
 
         // 1. dto를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+//        System.out.println(article.toString());
+        log.info(article.toString());
 
         // 2. 리파지토리로 엔티티를 db에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+//        System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
